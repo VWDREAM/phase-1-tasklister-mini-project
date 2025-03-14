4d262937ec7b3b0099d6bdbe777115a90705be05
VWDREAM <wanjiruvirginiah26@gmail.com>
2025-03-14 15:44:16 +0300

    taslister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const myForm = document.getElementById("create-task-form")
+  myForm.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    const taskInput = document.getElementById("new-task-description");
+  const taskText = taskInput.value.trim();
+
+  if (taskText === "") return;
+
+  const myList = document.getElementById("tasks")
+  const newTask = document.createElement("li");
+  newTask.textContent = taskText;
+  myList.appendChild(newTask);
+  taskInput.value = "";
+
+
+  });
+
+  });
+
+
+
